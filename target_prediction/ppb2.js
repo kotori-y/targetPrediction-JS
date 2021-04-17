007ad7999df1823c67166cf2da17aa1bf4c14505
Zhijiang Yang <yzjkid9@gmail.com>
2021-04-17 15:48:22 +0800

    :tada: finished the ppb2 spider

---
+++ target_prediction/ppb2.js
@@ -0,0 +1,106 @@
+/*
+ * @Description: predict the potential targets of query molecule through *ppb2*
+ * @Author: Kotori Y
+ * @Date: 2021-04-17 14:18:32
+ * @LastEditors: Kotori Y
+ * @LastEditTime: 2021-04-17 15:44:57
+ * @FilePath: \targetPrediction-JS\target_prediction\ppb2.js
+ * @AuthorMail: kotori@cbdd.me
+ *
+ * @url: https://ppb2.gdb.tools/#
+ */
+
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+
+
+async function PredictByPPB2(smiles, method = "NN(ECfp4)", idx = 1) {
+  console.log(`>>> waiting ${idx} ppb2-${method}...`);
+  var cols = ["Rank", "ChEMBL ID", "Common name", "Method", "pair"];
+  var urls = new Map([
+    [
+      "NN(ECfp4)",
+      `https://ppb2.gdb.tools/predict?smi=${smiles}&fp=ECfp4&method=Sim&scoringmethod=TANIMOTO`,
+    ],
+    [
+      "NN(ECfp4) + NB(ECfp4)",
+      `https://ppb2.gdb.tools/predict?smi=${smiles}&fp=ECfp4&method=SimPlusNaiveBayes&scoringmethod=TANIMOTO`,
+    ],
+    [
+      "NN(Xfp) + NB(ECfp4)",
+      `https://ppb2.gdb.tools/predict?smi=${smiles}&fp=Xfp&method=SimPlusNaiveBayes&scoringmethod=CBD`,
+    ],
+    [
+      "DNN(ECfp4)",
+      `https://ppb2.gdb.tools/predict?smi=${smiles}&fp=ECfp4&method=DNN&scoringmethod=TANIMOTO`,
+    ],
+  ]);
+
+  var out = [];
+
+  const url = urls.get(method);
+  // console.log(url)
+  try {
+    var resp = await fetch(url, { method: "GET", timeout: 180000 });
+    var html = await resp.text();
+
+    var $ = cheerio.load(html);
+
+    var reg = /(?<=CHEMBL\d+\=fld\=).*?(?=\s|')/g
+    var _simi = $(".showNNBtn")
+    console.log(typeof _simi.eq(0).attr('onclick'))
+    var simi = []
+    for (let i=0; i<=_simi.length-1; i++) {
+      simi.push(_simi.eq(i).attr('onclick').match(reg))
+    }
+    
+    console.log(simi)
+
+    var contents = $("#resultTable tr")
+      .text()
+      .trim()
+      .split("\n\n")
+      .map(ele =>
+        ele
+          .trim()
+          .split("\n")
+          .map((ele) => ele.trim())
+      );
+
+    var cols = contents[0];
+    var lines = contents.slice(1)
+    
+    lines.forEach((line, i) => line.push(idx, simi[i].join("|")))
+
+    cols[cols.length - 1] = "Method";
+    cols.push("idx", "simi");
+
+    for (let line of lines) {
+      var temp = {};
+      line[line.length - 3] = method;
+      for (var i = 0; i < line.length; i++) {
+        temp[cols[i]] = line[i];
+      }
+      out.push(temp);
+    }
+  } catch {
+    var temp = {};
+  }
+  
+  return {
+    cols: cols,
+    out: out,
+  };
+  
+}
+
+
+if (true) {
+  (async () => {
+    var smiles = "Oc1cc(CCl)ccc1Oc2ccc(Cl)cc2Cl";
+
+    const result = await PredictByPPB2(smiles, "NN(ECfp4)", "demo");
+    console.log(result);
+  })();
+}
+
